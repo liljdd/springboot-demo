c15a24f7afc19574fbbd8306c37f581eb62ec719
lilj <lilj@mchina.cn>
2018-08-03 14:17:36 +0800

    teacher

---
+++ src/main/java/com/ant/entity/Teacher.java
@@ -0,0 +1,66 @@
+package com.ant.entity;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Teacher {
+    private Integer id;
+
+    private String name;
+
+    private Boolean del;
+
+    private Short age;
+
+    private BigDecimal salary;
+
+    private Date createdtime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Boolean getDel() {
+        return del;
+    }
+
+    public void setDel(Boolean del) {
+        this.del = del;
+    }
+
+    public Short getAge() {
+        return age;
+    }
+
+    public void setAge(Short age) {
+        this.age = age;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+    public Date getCreatedtime() {
+        return createdtime;
+    }
+
+    public void setCreatedtime(Date createdtime) {
+        this.createdtime = createdtime;
+    }
+}
