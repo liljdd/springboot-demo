987c4b53e880a941b92cf9077dda55b9f0f65ace
lilj <lilj@mchina.cn>
2018-08-08 12:03:47 +0800

    teacher

--- src/main/java/com/ant/controller/StudentController.java
+++ src/main/java/com/ant/controller/StudentController.java
@@ -8,6 +8,9 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * @author lilj
@@ -25,6 +28,7 @@ public class StudentController {
     private StudentService service;
 
     @RequestMapping("detail")
+    @ResponseBody
     public JSONObject detail(Integer id){
         System.out.println("============"+id);
         Student bean = service.findById(id);
@@ -32,4 +36,11 @@ public class StudentController {
         System.out.println(s);
         return JSONObject.parseObject(s);
     }
+
+    @RequestMapping("all")
+    @ResponseBody
+    public List<Student> all(){
+        return service.findAll();
+    }
+
 }
--- src/main/java/com/ant/mapper/StudentMapper.java
+++ src/main/java/com/ant/mapper/StudentMapper.java
@@ -2,10 +2,15 @@ package com.ant.mapper;
 
 import com.ant.entity.Student;
 import com.ant.support.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
-
+@Mapper
+@Component
 public interface StudentMapper extends BaseMapper<Student> {
-    @Override
+
+    @Select("select * from student")
     List<Student> selectAll();
 }
